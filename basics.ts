8d8b55d6a7021e97086a542fc355295aacb921ec
JEE <jeerawut.jd40@gmail.com>
2024-02-04 11:30:08 +0700

    Basic Types

---
+++ basics.ts
@@ -0,0 +1,16 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+
+let age: number = 24;
+age = 12.1;
+
+let userName: string;
+userName = 'o_O';
+
+let isInstructor: boolean;
+isInstructor = true;
+
+let hobbies: null;
