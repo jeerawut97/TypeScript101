50a8f805d81f1f44d5c55f9ee198efef312334f2
JEE <jeerawut.jd40@gmail.com>
2024-02-04 11:36:01 +0700

    Working with Array & Object

--- basics.ts
+++ basics.ts
@@ -3,7 +3,6 @@
 // Function types, parameters
 
 // Primitives
-
 let age: number = 24;
 age = 12.1;
 
@@ -13,4 +12,25 @@ userName = 'o_O';
 let isInstructor: boolean;
 isInstructor = true;
 
-let hobbies: null;
+// More complex types
+let hobbies: string[];
+hobbies = ['Sports', 'Cooking'];
+
+let person: {
+    name: string;
+    age: number;
+};
+
+person = {
+    name: 'Max',
+    age: 32
+}
+
+// person = {
+//     isEmployee: true
+// }
+
+let people: {
+    name: string;
+    age: number;
+}[];
